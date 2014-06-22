3d51ba567f93f8e0ffd98d05550d731519f5b034
boboman13 <rich@safe-node.com>
2014-06-22 15:50:31 -0700

    This is just my preference

--- src/genpassword.c
+++ src/genpassword.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "genpassword.h"
 
-char* generate_password(int length) {
+char *generate_password(int length) {
 	char *pool = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 	char *password = malloc( sizeof(*password) * (length + 1) );
 
